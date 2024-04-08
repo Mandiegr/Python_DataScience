663986693e3973f7217fb29bd98a488e66cb1c2d
Mandiegr <nannda076@gmail.com>
2024-04-08 13:52:42 -0300

    feat: Comparison of X and Y bar chart

--- python/graficos.py
+++ python/graficos.py
@@ -29,3 +29,25 @@ plt.ylabel("Eixo y")
 
 plt.bar(x,y)
 plt.show()
+
+#Comparação de duas barras X e Y
+
+x1 = [1, 3, 5, 7, 9]
+y1 = [2, 3, 7, 1, 0]
+
+x2 = [2, 4, 6, 8, 10]
+y2 = [5, 1, 3, 7, 4]
+
+titulo = "Gráfico em barras 2"
+eixox = "Eixo X"
+eixoy = "Eixo Y"
+
+plt.title(titulo)
+plt.xlabel("Eixo X")
+plt.ylabel("Eixo y")
+
+plt.bar(x1,y1, label = "Grupo 1")
+plt.bar(x2,y2, label = "Grupo 2")
+plt.legend()
+
+plt.show()
