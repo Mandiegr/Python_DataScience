4732178f967472d8d710b2b38a3d9bb4fdf010b6
Mandiegr <nannda076@gmail.com>
2024-04-08 13:37:25 -0300

    feat: data visualization: bar chart

--- python/graficos.py
+++ python/graficos.py
@@ -13,3 +13,19 @@ plt.ylabel("Eixo y")
 
 plt.plot(x,y)
 plt.show()
+
+#Gráfico em barra
+
+x = [1, 2, 3, 4, 5]
+y = [2, 3, 7, 1, 0]
+
+titulo = "Gráfico em barras"
+eixox = "Eixo X"
+eixoy = "Eixo Y"
+
+plt.title(titulo)
+plt.xlabel("Eixo X")
+plt.ylabel("Eixo y")
+
+plt.bar(x,y)
+plt.show()
