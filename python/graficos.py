b14e9ae52dbe4d68a8e19cd2b52c9db8a55ce315
Mandiegr <nannda076@gmail.com>
2024-04-09 14:39:30 -0300

    feat: Scatterplot: points

--- python/graficos.py
+++ python/graficos.py
@@ -3,6 +3,7 @@
 
 import matplotlib.pyplot as plt
 
+
 x = [1, 2, 5]
 y = [2, 3, 7]
 
@@ -51,3 +52,124 @@ plt.bar(x2,y2, label = "Grupo 2")
 plt.legend()
 
 plt.show()
+
+# Scatterplot: Gráfico de dispersão
+
+x = [1, 3, 5, 7, 9]
+y = [2, 3, 7, 1, 0]
+
+
+titulo = "Scatterplot: Gráfico de dispersão"
+eixox = "Eixo X"
+eixoy = "Eixo Y"
+
+plt.title(titulo)
+plt.xlabel("Eixox")
+plt.ylabel("Eixoy")
+
+plt.scatter(x, y, label="Meus pontos", color="r")
+plt.plot(x, y)
+plt.legend()
+
+plt.show()
+
+"""
+Documentação oficial do Matplotlib
+A seguir, alguns exemplos de argumentos que podem ser aplicados ao método plot( ).
+
+
+
+color: cor (ver exemplos abaixo)
+
+label: rótulo
+
+linestyle: estilo de linha (ver exemplos abaixo)
+
+linewidth: largura da linha
+
+marker: marcador (ver exemplos abaixo)
+
+
+
+CORES (color)
+'b' blue
+
+'g' green
+
+'r' red
+
+'c' cyan
+
+'m' magenta
+
+'y' yellow
+
+'k' black
+
+'w' white
+
+
+
+Marcadores (marker)
+'.' point marker
+
+',' pixel marker
+
+'o' circle marker
+
+'v' triangle_down marker
+
+'^' triangle_up marker
+
+'<' triangle_left marker
+
+'>' triangle_right marker
+
+'1' tri_down marker
+
+'2' tri_up marker
+
+'3' tri_left marker
+
+'4' tri_right marker
+
+'s' square marker
+
+'p' pentagon marker
+
+'*' star marker
+
+'h' hexagon1 marker
+
+'H' hexagon2 marker
+
+'+' plus marker
+
+'x' x marker
+
+'D' diamond marker
+
+'d' thin_diamond marker
+
+'|' vline marker
+
+'_' hline marker
+
+
+
+
+
+Tipos de linha (linestyle)
+'-' solid line style
+
+'--' dashed line style
+
+'-.' dash-dot line style
+
+':' dotted line style
+
+
+
+Fonte: https://matplotlib.org/api/_as_gen/matplotlib.pyplot.plot.htm
+
+"""
