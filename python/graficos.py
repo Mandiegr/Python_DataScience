2e6185566b8e9276c585d1af08ca5c6efe3588ca
Mandiegr <nannda076@gmail.com>
2024-04-09 23:32:16 -0300

    feat: Activity: creating a population growth graph

--- python/graficos.py
+++ python/graficos.py
@@ -3,6 +3,7 @@
 
 import matplotlib.pyplot as plt
 
+
 x = [1, 2, 5]
 y = [2, 3, 7]
 
@@ -92,6 +93,28 @@ plt.scatter(x, y, label="Meus pontos", color="k", marker=".", s=z)
 plt.legend()
 plt.show()
 plt.savefig("figura1.png", dpi=300) # salva o gráfico na pasta em que foi criado o projeto
+
+# Atividade:  Estudo de caso: crescimento da população brasileira
+
+dados = open("populacao_brasileira.csv").readlines()
+
+x = []
+y = []
+
+for i in range(len(dados)):
+	if i != 0:
+		linha = dados[i].split(";")
+		x.append(int(linha[0]))
+		y.append(int(linha[1]))
+
+plt.bar(x, y, color="#e4e4e4")
+plt.plot(x, y, color='k', linestyle="--")
+
+plt.title("Crescimento da População Brasileira 1980-2016")
+plt.xlabel("Ano")
+plt.ylabel("População x100.000.000")
+#plt.show()
+plt.savefig("populacao_brasileira.png", dpi=300)
 
 
 """
